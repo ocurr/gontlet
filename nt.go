399ecae17f75e2fb3b99d41b39b147a5bba7603a
ocurr <owcurry42@gmail.com>
2016-02-06 22:56:28 -0800

    built the public interface for gontlet

---
+++ nt.go
@@ -0,0 +1,37 @@
+package gontlet
+
+import (
+	"fmt"
+)
+
+var (
+	tableList map[string]*Table
+	server *Server
+)
+
+func Init() {
+	tableList = make(map[string]*Table)
+	server = newServer("8081")
+	go server.serve()
+	go updateServer()
+}
+
+func GetTable(tableName string) *Table {
+	if tableList[tableName] == nil {
+		tableList[tableName] = &Table{Pairs: make([]*Entry, 0)}
+	}
+	return tableList[tableName]
+}
+
+func updateServer() {
+	for {
+		for k,v := range tableList {
+			for _,entry := range v.Pairs {
+				if entry.Updated {
+					server.sendOutgoing([]byte(k+";"+entry.Name+"="+entry.Value))
+					entry.Updated = false
+				}
+			}
+		}
+	}
+}
