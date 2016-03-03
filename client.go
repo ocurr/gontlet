987b637699fc61b7e1ce8256e6d40b120869c0d8
ocurr <owcurry42@gmail.com>
2016-03-03 10:02:57 -0800

    added interface specifics to client.go and made an attempt to find a new connection upon losing the old one

--- client.go
+++ client.go
@@ -1,40 +1,47 @@
 package gontlet
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"time"
 )
 
 type Client struct {
-	conn    connection
-	send    chan []byte
-	recv    chan []byte
-	address string
+	conn       connection
+	send       chan []byte
+	recv       chan []byte
+	unregister chan connection
+	address    string
 }
 
 func newClient(address string) *Client {
 	c := &Client{
-		send:    make(chan []byte, 1),
-		recv:    make(chan []byte, 1),
-		address: address,
+		send:       make(chan []byte, 1),
+		recv:       make(chan []byte, 1),
+		unregister: make(chan connection, 1),
+		address:    address,
 	}
 	c.connect()
 	return c
 }
 
 func (c *Client) connect() {
+	var err error
 	addr, err := net.ResolveTCPAddr("tcp", c.address)
 	if err != nil {
 		fmt.Println("Error on client address:", err)
 	}
-	for conn, err := net.DialTCP("tcp", nil, addr); ; {
-		if err != nil {
-			time.Sleep(4 * time.Second)
-		} else {
-			c.conn = connection{conn: conn, send: make(chan []byte, 1)}
-		}
+	var conn net.Conn
+	err = errors.New("dummy error")
+	for err != nil {
+		conn, err = net.DialTCP("tcp", nil, addr)
+		fmt.Println("unable to find a connection")
+		time.Sleep(4 * time.Second)
 	}
+	fmt.Println("found connection")
+	c.conn = connection{conn: conn, send: make(chan []byte, 1)}
+	go c.conn.handle(c)
 }
 
 func (c *Client) sendOutgoing(data []byte) {
@@ -45,9 +52,19 @@ func (c *Client) getIncoming() []byte {
 	return <-c.recv
 }
 
+func (c *Client) recieve(buff []byte) {
+	c.recv <- buff
+}
+
+func (c *Client) unregisterConn(conn connection) {
+	c.unregister <- conn
+}
+
 func (c *Client) serve() {
 	for {
 		select {
+		case <-c.unregister:
+			c.connect()
 		case msg := <-c.send:
 			c.conn.sendData(msg)
 		}
